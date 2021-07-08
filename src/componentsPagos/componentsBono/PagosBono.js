acf676751758a2d2f6bd38e034f93aaa8c5571f0
Alejandro <alejandro.rudolphy@alumnos.uv.cl>
2021-07-08 16:19:45 -0400

    Al día

--- src/componentsPagos/componentsBono/PagosBono.js
+++ src/componentsPagos/componentsBono/PagosBono.js
@@ -1,37 +1,75 @@
-//En este componente está la tabla con info de los pagos de afiliados.
+//Aquí se muestra todo lo que tiene que ver con los ingresos
+//de la asociación distintos de los pagos que hacen los afiliados
+//Ejemplo: Subvenciones.
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-import { BorderAll } from '@material-ui/icons';
-import Box from '@material-ui/core/Box';
+import AgregarPago from '../../components/Buttons/Agregar';
+import { useState } from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { orange, purple } from '@material-ui/core/colors';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  fab: {
+    margin: theme.spacing(2)
+  },
+}));
 
 const columns = [
-  { field: 'id', headerName: 'RUT', width: 150 },
-  { field: 'firstName', headerName: 'Nombre', width: 200 },
-  { field: 'lastName', headerName: 'Apellido', width: 200 },
+  { field: 'id', headerName: 'ID', width: 130 },
+  { field: 'nombreIngreso', headerName: 'Nombre', width: 130 },
+  { field: 'montoIngreso', headerName: 'Monto', type: 'number', width: 130 },
+  { field: 'fechaIngreso', headerName: 'Fecha', width: 130, type: 'date'},
+  { field: 'action', headerName: 'Accion', width: 120, renderCell: (params: GridCellParams)=> (
+    <strong>
+      <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton aria-label="edit">
+        <EditIcon />
+      </IconButton>
+    </strong>
+  )}
 ];
 
 const rows = [
-  { id: '19817247-2', firstName: 'Ana', lastName: 'Muñoz' },
-  { id: '19772624-5', firstName: 'Alejandro', lastName: 'Oliveros'},
-  { id: '19490110-0', firstName: 'Alejandro', lastName: 'Rudolphy'},
+  { id: '1', nombreIngreso: 'Subvencion Municipal', montoIngreso: '500000', fechaIngreso: '01/03/2021'},
+  { id: '2', nombreIngreso: 'Subvencion Municipal', montoIngreso: '500000', fechaIngreso: '01/04/2021'},
+  { id: '3', nombreIngreso: 'Subvencion Municipal', montoIngreso: '500000', fechaIngreso: '01/05/2021'},
+  { id: '4', nombreIngreso: 'Subvencion Municipal', montoIngreso: '500000', fechaIngreso: '01/08/2021'},
 ];
 
-const mystyle = {
-  height: 400,
-  width: "100%",
-  //padding: "10px",
-  margin: '0px 450px 10px',
-};
 
-function PagosBonos() {
+function PagosBono() {
+  
+  const classes = useStyles();
   return (
-    <Box display="flex" justifyContent="flex-end" m={1} p={0} > 
-    <div style={mystyle}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
-    </Box>
+  <div style={{ height: 400, width: '100%' }}>
+    <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+  </div>
   );
+  
 }
 
-export default PagosBonos;
+
+export default PagosBono;
 
