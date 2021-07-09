612784d50beb574f7034f53ad7db6d4b5da3a8fc
FelipeRojas1998 <felipe.rojasg@alumnos.uv.cl>
2021-07-09 17:53:35 -0400

    .

--- src/pages/Información.js
+++ src/pages/Información.js
@@ -1,113 +1,91 @@
+
+
 //import React from 'react'
 import Navbar from "../componentsConvenio/Navbar";
 
+import React from 'react'
+import { Item, Icon, Container, Header, Grid, Segment, Image } from 'semantic-ui-react'
 
 
-import _ from 'lodash'
-import React, { Component } from 'react'
-import { Button, Card, Divider, Image, Placeholder, Icon, List } from 'semantic-ui-react'
+const ItemExampleContents = () => (
+  <center>
+    <div>
+    <Container fluid>
+      <Header as='h2'>  Contactos Recomendados</Header>
+      <p>
+        
+      </p>
+      
+    </Container>
+  </div>
 
-const cards = [
-  {
-    width: '300%',
-    top: '400%',
-    Icon: 'id badge',
-    date: 'Joined in 2013',
-    header: 'Helen',
-    description: 'Primer Contacto' ,
-  },
-  {
-    Icon: 'id badge',
-    date: 'Joined in 2013',
-    header: 'Matthew',
-    description: 'Segundo Contacto ',
-    
-  },
-  {
-    Icon: 'id badge',
-    date: 'Joined in 2013',
-    header: 'Molly',
-    description: 'Tercer Contacto',
-  },
-]
-
-export default class PlaceholderExampleCard extends Component {
-  state = { loading: false }
-
-  handleLoadingClick = () => {
-    this.setState({ loading: true })
-
-    setTimeout(() => {
-      this.setState({ loading: false })
-    }, 3000)
-  }
-
-  render() {
-    const { loading } = this.state
-
-    return (
-      <>
-
-      <center>
-        <Button loading={loading} onClick={this.handleLoadingClick} primary>
-          Actualizar
-        </Button>
-        <Divider />
-
-        <Card.Group doubling itemsPerRow={1} stackable>
-          {_.map(cards, (card) => (
-            <Card key={card.header}>
-              {loading ? (
-                <Placeholder>
-                  <Placeholder.Image square />
-                </Placeholder>
-              ) : (
-                <Image src={card.avatar} />
-              )}
-
-              <Card.Content>
-                {loading ? (
-                  <Placeholder>
-                    <Placeholder.Header>
-                      <Placeholder.Line length='very short' />
-                      <Placeholder.Line length='medium' />
-                    </Placeholder.Header>
-                    <Placeholder.Paragraph>
-                      <Placeholder.Line length='short' />
-                    </Placeholder.Paragraph>
-                    
-                  </Placeholder>
-                ) : (
-                  <>
-                    <Card.Header>{card.header}</Card.Header>
-                    <Card.Meta>{card.date}</Card.Meta>
-                    <Card.Description>{card.description}</Card.Description>
-                    
-                  </>
-                )}
-              </Card.Content>
-
-              <Card.Content extra>
+  <Grid.Column width={6}>
+        <Segment>
+        <Item>
+    <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Item.Content verticalAlign='middle'> 
+      
+      <Icon name='phone' size='large' />
+                :+56975447793
+                <div>
+                <Icon name='envelope outline' size='large' />
+                <a href='mailto:jack@semantic-ui.com'>:feliperojasgonzalez@hotmail.com</a>
                 
-                <Icon name='phone' size='large' />
+                </div> 
+      
+      </Item.Content>  
+    
+    </Item>
+  
+        </Segment>
+
+        <Segment>
+
+        <Item>
+    <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Item.Content verticalAlign='middle'> 
+      
+      <Icon name='phone' size='large' />
                 :+56975447793
                 <div>
                 <Icon name='envelope outline' size='large' />
                 <a href='mailto:jack@semantic-ui.com'>:feliperojasgonzalez@hotmail.com</a>
                 
 
-                </div>
-  
-                
-              </Card.Content>
+                </div> 
+      
+      </Item.Content>  
+    
+    </Item>
 
-              
-            </Card>
-          ))}
-        </Card.Group>
-        </center>
-        <Navbar/>
-      </>
-    )
-  }
-}
+        </Segment>
+
+        <Segment>
+        <Item>
+    <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Item.Content verticalAlign='middle'> 
+      
+      <Icon name='phone' size='large' />
+                :+56975447793
+                <div>
+                <Icon name='envelope outline' size='large' />
+                <a href='mailto:jack@semantic-ui.com'>:feliperojasgonzalez@hotmail.com</a>
+                
+
+                </div> 
+      
+      </Item.Content>  
+    
+    </Item>
+    
+        </Segment>
+      </Grid.Column>
+
+  <Navbar/>
+
+  </center>
+  
+)
+
+
+export default ItemExampleContents
