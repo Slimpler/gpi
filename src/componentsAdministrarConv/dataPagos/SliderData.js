6bc589913bb41d15266078ea971f3e37076fa5d3
SebastianGonzalezMorales <sebastian.gonzalez@alumnos.uv.cl>
2021-07-12 17:39:55 -0400

    Subiendo Administración de convenios

---
+++ src/componentsAdministrarConv/dataPagos/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from "../../img/house-1.jpg";
+import ImageTwo from "../../img/house-2.jpg";
+import ImageThree from "../../img/house-3.jpg";
+import ImageFour from "../../img/house-4.jpg";
+
+export const SliderData = [
+  {
+    title: "Camping",
+    price: "$10.000",
+    path: "/homes",
+    label: "Quintero Camping",
+    image: ImageOne,
+    alt: "Convenios",
+  },
+  {
+    title: "Caja Los Andes",
+    price: "$50.000",
+    path: "/homes",
+    label: "Caja Los Andes",
+    image: ImageTwo,
+    alt: "Convenios",
+  },
+  {
+    title: "Optica Viña del Mar ",
+    price: "$20.000",
+    path: "/homes",
+    label: "Optica Viña del Mar",
+    image: ImageThree,
+    alt: "Convenios",
+  },
+  {
+    title: "Coopeuch",
+    price: "$10.000",
+    path: "/homes",
+    label: "Coopeuch",
+    image: ImageFour,
+    alt: "Convenios",
+  },
+];
