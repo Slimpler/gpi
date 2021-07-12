8ac981b439a4d35a9260a5f8fe5382c7f1fa5b60
Alejandro <alejandro.rudolphy@alumnos.uv.cl>
2021-07-12 16:51:24 -0400

    Actualizacion NavBar

--- src/componentsPagos/dataPagos/InfoData.js
+++ src/componentsPagos/dataPagos/InfoData.js
@@ -5,12 +5,7 @@ import ImageThree from "../../img/house-5.jpg";
 export const InfoData = {
   heading2: "Pagos de la Asociación",
   heading: "Pagos de Afiliados",
-  paragraphOne:
-    "Breve descripción",
-  paragraphTwo:
-    "Instrucción",
   buttonLabel: "Ir!",
-  image: ImageOne,
   reverse: false,
   delay: 100,
 
@@ -19,37 +14,8 @@ export const InfoData = {
 export const InfoDataTwo = {
   heading2: "Pagos de prestamos",
   heading: "Pagos de bonos",
-  paragraphOne:
-    "Breve descripción",
-  paragraphTwo:
-    "Instrucción",
   buttonLabel: "Ir!",  
-  image: ImageTwo,
   reverse: false,
-  delay: 300,
-};
-
-export const InfoDataThree = {
-  heading: "Bonos",
-  paragraphOne:
-    "descripción",
-  paragraphTwo:
-    "Instrucción",
-  buttonLabel: "Ir!",
-  image: ImageThree,
-  reverse: false,
-  delay: 300,
-};
-
-export const InfoDataFour = {
-  heading: "Préstamos",
-  paragraphOne:
-    "descripción",
-  paragraphTwo:
-    "Instrucción",
-  buttonLabel: "Ir!",
-  image: ImageThree,
-  reverse: true,
   delay: 300,
 };
 
