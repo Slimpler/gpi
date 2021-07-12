baf8d2ea0e0e45869021e688513e6f64ba05681b
ana-munoz <ana.munoz@alumnos.uv.cl>
2021-07-12 17:40:58 -0400

    arreglo de contenido de tablas

--- src/componentsPagos/componentsBono/PagosBonoAfiliado.js
+++ src/componentsPagos/componentsBono/PagosBonoAfiliado.js
@@ -12,29 +12,29 @@ const columns = [
   { field: 'firstName', headerName: 'Nombre', width: 150 },
   { field: 'lastName', headerName: 'Apellido', width: 150 },
   { field: 'tipoBono', headerName: 'Bono', width: 150},
-  { field: 'estado', headreName: 'Estado', width: 200},
+  { field: 'estado', headreName: 'Estado', width: 150},
   { field: 'montoBono', headerName: 'Monto', type: 'number', width: 150}
 ];
 
 const rows = [
-  { id: '1', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
-  { id: '2', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
-  { id: '3', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
-  { id: '4', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
-  { id: '5', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
-  { id: '6', firstName: 'Ana', lastName: 'Muñoz', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
+  { id: '1', firstName: 'José', lastName: 'Coronado', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
+  { id: '2', firstName: 'José', lastName: 'Coronado', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
+  { id: '3', firstName: 'José', lastName: 'Coronado', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
+  { id: '4', firstName: 'José', lastName: 'Coronado', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
+  { id: '5', firstName: 'José', lastName: 'Coronado', tipoBono: 'Navidad',  estado: 'Disponible', montoBono: '100000'},
+  { id: '6', firstName: 'José', lastName: 'Coronado', tipoBono: 'Fiestas Patrias',  estado: 'Disponible', montoBono: '100000'},
 
 ];
 
 const mystyle = {
   height: 400,
-  width: "100%",
+  width: "60%",
   //padding: "10px",
   margin: '0px 250px 10px',
 };
 const textStyle = {
   width: '100%',
-  margin: '10px 100px 10px',
+  margin: '10px 100px 20px',
 };
 
 function PagosBonos() {
@@ -42,10 +42,10 @@ function PagosBonos() {
       <div >
         <div style={textStyle}>
           <Typography variant="h4" gutterBottom>
-            TEST DE TÍTULO
+            Mis Bonos
           </Typography>
           <Typography variant="body1" gutterBottom>
-            Test de descripcion tipo body1
+            En esta tabla se pueden ver los bonos que se le han sido asignados con sus respectivos montos y disponibilidad.
           </Typography>
         </div>
         <div >
